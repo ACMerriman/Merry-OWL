1b8a080010a8bf1127cff4cbf2ed94dd7e97c072
ACMerriman <acmerriman94@gmail.com>
2018-02-01 18:16:56 -0500

    asd

--- js/Node/server.js
+++ js/Node/server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/../.."));
 
 app.get("/*", function(req, res)
 {
-	res.sendFile(path.join(__dirname+"/../../view/index.html"));
+	res.sendFile(path.join(__dirname+"/../../build/index.html"));
 });
 
 app.listen(process.env.PORT || 8082);
